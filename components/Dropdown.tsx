56afa8cf8929bbb272ef7f829d79bb92c655cfc7
berbaroovez <carterolson3@gmail.com>
2022-01-21 02:59:57 -0600

    removed button

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -9,19 +9,19 @@ export default function DropdownBasic({ username }: Props) {
   const [checked, setChecked] = useState(false);
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger>
-        <button className="bg-slate-600 p-2 rounded">{username}</button>
+      <DropdownMenu.Trigger className="bg-slate-600 p-2 rounded text-white">
+        {username}
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Content className="bg-slate-600 p-4 rounded text-white text-center">
         <DropdownMenu.Item className="hover:bg-gray-600">
           <Link href="/dashboard">Dashboard</Link>
         </DropdownMenu.Item>
-        <DropdownMenu.Separator className="h-px bg-slate-100 m-2" />
+        <DropdownMenu.Separator className="h-px bg-slate-300 m-2" />
         <DropdownMenu.Item>
           <Link href="/leaderboards">Leaderboards</Link>
         </DropdownMenu.Item>
-        <DropdownMenu.Separator className="h-px bg-slate-100 m-2" />
+        <DropdownMenu.Separator className="h-px bg-slate-300 m-2" />
         <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
