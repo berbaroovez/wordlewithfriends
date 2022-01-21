d8f543ccd9d82d4c5086085974ffd4e6111967e6
berbaroovez <carterolson3@gmail.com>
2022-01-21 02:56:17 -0600

    switched to radix dropdown

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,16 +1,6 @@
-import {
-  Button,
-  Divider,
-  Dropdown,
-  IconClipboard,
-  IconCopy,
-  IconLogOut,
-  IconTrash,
-  Typography,
-} from "@supabase/ui";
 import Link from "next/link";
 import { useState } from "react";
-
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 interface Props {
   username: string;
 }
@@ -18,28 +8,22 @@ interface Props {
 export default function DropdownBasic({ username }: Props) {
   const [checked, setChecked] = useState(false);
   return (
-    <Dropdown
-      overlay={[
-        <Dropdown.Item icon={<IconClipboard />} key={1}>
-          {/* <Typography.Text>Dashboard</Typography.Text> */}
-          <Link href="/dashboard">
-            <a>Dashboard</a>
-          </Link>
-        </Dropdown.Item>,
-        <Dropdown.Item icon={<IconCopy />} key={2}>
-          <Typography.Text key={3}>Settings</Typography.Text>
-        </Dropdown.Item>,
-        <Divider light key={4} />,
-        <Dropdown.Item key={5} icon={<IconLogOut />}>
-          <Typography.Text key={6}>Log out</Typography.Text>
-        </Dropdown.Item>,
-        <Divider light key={7} />,
-        <Dropdown.Checkbox checked={checked} onChange={setChecked} key={8}>
-          <Typography.Text key={9}>Color Blind</Typography.Text>
-        </Dropdown.Checkbox>,
-      ]}
-    >
-      <Button className="bg-orange-400">{username}</Button>
-    </Dropdown>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <button className="bg-slate-600 p-2 rounded">{username}</button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="bg-slate-600 p-4 rounded text-white text-center">
+        <DropdownMenu.Item className="hover:bg-gray-600">
+          <Link href="/dashboard">Dashboard</Link>
+        </DropdownMenu.Item>
+        <DropdownMenu.Separator className="h-px bg-slate-100 m-2" />
+        <DropdownMenu.Item>
+          <Link href="/leaderboards">Leaderboards</Link>
+        </DropdownMenu.Item>
+        <DropdownMenu.Separator className="h-px bg-slate-100 m-2" />
+        <DropdownMenu.Item>Sign Out</DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
